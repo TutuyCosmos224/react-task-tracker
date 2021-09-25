d936dde22bc0290f0bdc43274b6a872a576bc5ce
Hansen Emanuel <hansenemanuel@hotmail.com>
2021-09-26 00:48:23 +0700

    simulate backend using json server

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,7 +7,7 @@ const Task = ({ task, onDelete, onToggle }) => {
             onDoubleClick={() => onToggle(task.id)} >
             <h3>
                 {task.text} <FaTimes style = {{color: 
-                'red', cursor: 'pointer' }} onClick={onDelete} />
+                'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} />
             </h3>
             <p>{task.day}</p>
         </div>
